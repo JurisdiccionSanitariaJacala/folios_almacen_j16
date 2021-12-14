82615e081debff849bf50e369c33c34c214b361f
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-12-13 22:32:53 -0600

    .
    .

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Folios Almacen J16',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Folios Almacen J16 Demo'),
@@ -31,15 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -62,7 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
         //codigo para cambiar de actividad
         case "PRINCIPAL":
           break;
-        default: break;
+        default: 
+          break;
       }      
     });
   }
@@ -77,14 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'SALIDA DE VALES DE ALMACEN JURISDICCIONAL'
-            ),
+            const Text('SALIDA DE VALES DE ALMACEN JURISDICCIONAL'),
             Column(
               children: <Widget>[
-                //Row(children: <Widget>[
-                //  ElevatedButton(onPressed: () {},
-                //  child: const Text('VALE 1'))],),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "DSDF"
+                  ),
+                ),
                 ElevatedButton(
                   onPressed: () => { }, //metodo para cambiar a otra pagina
                   child: const Text('GENERAR VALE')
