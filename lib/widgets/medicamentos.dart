784603bc6190791fe73248f3c24d4dcd19dc2c43
Christian Yamil Castillo <cyccov@gmail.com>
2022-02-02 16:12:46 -0600

    Update medicamentos.dart

--- lib/widgets/medicamentos.dart
+++ lib/widgets/medicamentos.dart
@@ -3,45 +3,44 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
-class Medicamentos{
+class Medicamentos {
   String? keyword;
   int? id;
   String? autoCompleteTerm;
   String? presentacion;
   String? clave;
 
-  Medicamentos({
-    this.keyword,
-    this.autoCompleteTerm, 
-    this.id, 
-    this.clave, 
-    this.presentacion
-  });
+  Medicamentos(
+      {this.keyword,
+      this.autoCompleteTerm,
+      this.id,
+      this.clave,
+      this.presentacion});
 
   factory Medicamentos.fromJson(Map<String, dynamic> parsedJson) {
     return Medicamentos(
-      keyword: parsedJson["keyword"] as String,
-      id: parsedJson["id"] as int,
-      autoCompleteTerm: parsedJson["autoCompleteTerm"] as String,
-      clave: parsedJson["clave"] as String,
-      presentacion: parsedJson["presentacion"] as String
-    );
+        keyword: parsedJson["keyword"] as String,
+        id: parsedJson["id"] as int,
+        autoCompleteTerm: parsedJson["autoCompleteTerm"] as String,
+        clave: parsedJson["clave"] as String,
+        presentacion: parsedJson["presentacion"] as String);
   }
 }
 
-class MedicamentosViewModel{
+class MedicamentosViewModel {
   static List<Medicamentos>? medicamentos;
 
-  static Future loadMedicamentos() async{
-    try{
+  static Future loadMedicamentos() async {
+    try {
       medicamentos = [];
-      String jsonString = await rootBundle.loadString('assets/claves.json'); //carga JSON
+      String jsonString =
+          await rootBundle.loadString('assets/claves.json'); //carga JSON
       Map parsedJson = json.decode(jsonString);
       var categoryJson = parsedJson['medicamentos'] as List;
-      for(int i = 0; i < categoryJson.length; i++){
+      for (int i = 0; i < categoryJson.length; i++) {
         medicamentos?.add(Medicamentos.fromJson(categoryJson[i]));
       }
-    } catch(e) {
+    } catch (e) {
       print(e);
     }
   }
